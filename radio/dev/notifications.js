d7b22d1576adb797998486e28e3d5954ee187851
elettro <dpalermo@elettro.com>
2026-07-21 15:27:21 -0400

    Decouple mobile critical fixes from notification bootstrap

--- radio/dev/notifications.js
+++ radio/dev/notifications.js
@@ -50,6 +50,5 @@
       loadScript('./notifications-core.js?v=20260721-compact3'),
       loadScript('./mobile-ux-phase2.js?v=20260721-phase2b'),
       loadScript('./mobile-notification-peek.js?v=20260721-peek2')
-    ]).then(() => loadScript('./mobile-functional-repair.js?v=20260721-repair2'))
-      .catch((error) => console.error('[notifications] DEV notification client failed', error)));
+    ]).catch((error) => console.error('[notifications] DEV notification client failed', error)));
 })();
