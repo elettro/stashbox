1b9c9bcb7dbead041f35dd76ad35711c80b1ecd2
elettro <dpalermo@elettro.com>
2026-07-29 10:44:55 -0400

    Refresh Song CMS ZIP upload fix

--- radio-admin/dev/app.js
+++ radio-admin/dev/app.js
@@ -9,8 +9,8 @@
 
   if (window.location.pathname.includes('/radio-admin/songs/dev')) {
     document.write('<script src="/radio-admin/songs/dev/vec-disabled.js?v=20260728-songs-vec-disabled1"><\/script>');
-    document.write('<script src="/radio-admin/songs/dev/song-images-presign-route.js?v=20260728-song-images-storage1"><\/script>');
+    document.write('<script src="/radio-admin/songs/dev/song-images-presign-route.js?v=20260729-song-images-storage2"><\/script>');
     document.write('<script src="/radio-admin/songs/dev/song-images.js?v=20260729-song-images-order1"><\/script>');
-    document.write('<script src="/radio-admin/songs/dev/song-images-zip.js?v=20260729-song-images-zip1"><\/script>');
+    document.write('<script src="/radio-admin/songs/dev/song-images-zip.js?v=20260729-song-images-zip2"><\/script>');
   }
 })();
